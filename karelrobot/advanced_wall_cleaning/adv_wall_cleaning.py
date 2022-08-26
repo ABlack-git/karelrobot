913b689cce5d662983c9f172d58b58485b537515
Andrew Zakharchenko <andrew.zakharchenko@merck.com>
2022-08-26 19:58:14 +0200

    Add advanced wall cleaning

---
+++ karelrobot/advanced_wall_cleaning/adv_wall_cleaning.py
@@ -0,0 +1,66 @@
+from stanfordkarel import *
+from enum import Enum
+
+
+class Orientation(Enum):
+    NORTH = 1
+    EAST = 2
+    WEST = 3
+    SOUTH = 4
+
+
+x_coord = 0
+y_coord = 0
+current_orientation = Orientation.NORTH
+
+
+def step():
+    """ Implement and use this function instead of move. This function should help you update coordinates while you
+    move"""
+    pass
+
+
+def update_coordinates(direction: Orientation):
+    """Update x and y coordinates depending on the direction in which you move"""
+    pass
+
+
+def rotate_left():
+    """ Implement and use this function instead of turn_left. This function should help you keep track of orientation
+    when you turn Karel"""
+    pass
+
+
+def do_steps(number_of_steps):
+    pass
+
+
+def clean_wall():
+    """Clean one wall to the nearest corner"""
+    pass
+
+
+def clean_beeper():
+    """
+    Use corner_color_is(color) function to check the color of a corner. Possible values for color are RED, GREEN, BLUE,
+    YELLOW, BLACK.
+
+    When you determined the color of the corner move beeper according to this rules:
+
+    If corner is RED pick beeper, face north and move until there are no walls on left and right, put beeper.
+    If corner is GREEN pick beeper, face east and move until there are no walls on left and right, put beeper.
+    If corner is BLUE pick beeper, face west and move until there are no walls on left and right, put beeper.
+    If corner is YELLOW pick beeper, face south and move until there are no walls on left and right, put beeper.
+    If corner is BLACK just pick beeper.
+    If corner doesn't have any of the mentioned above colors just move beeper away from wall as in the previous
+    assignment
+    """
+    pass
+
+
+def main():
+    """ Write your main program """
+
+
+if __name__ == '__main__':
+    run_karel_program("20x15_adv_wall_cleaning")
--- karelrobot/wall_cleaning/wall_cleaning.py
+++ karelrobot/wall_cleaning/wall_cleaning.py
@@ -4,7 +4,7 @@ from stanfordkarel import *
 wall). The room has a rectangular shape and doesn't have any obstacles. There are no beepers in the corner. Initial 
 position of Karel will be always near a wall but not always in corner. Start by going to the nearest corner in the 
 direction where Karl is facing, when you get to the nearest corner position Karl such that he has a wall on the right
-and behind him. Clean the walls. In the end Karel should be in the corner of the last wall you ckeanedd."""
+and behind him. Clean the walls. In the end Karel should be in the corner of the last wall you cleaned."""
 
 
 def go_to_corner():
@@ -21,4 +21,4 @@ def main():
 
 
 if __name__ == '__main__':
-    run_karel_program("5_by_5_move_beepers")
+    run_karel_program("5x5_move_beepers")
